3c79d397d5e66b90ef7916649f50820dcf7c3cf6
Bola Magdy <bolamagdy085@gamil.com>
2023-06-26 21:12:51 +0300

    Home Controller

---
+++ Final_Project/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy() 
+        {
+            return View(); 
+        }
+    }
+}
