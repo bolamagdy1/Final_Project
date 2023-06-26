265cb69c6c890c44a7398ae6185ba5aaf47e26a9
Abdalrhman <abdalrhmanyasser@icloud.com>
2023-06-26 21:16:00 +0300

    nw cont

---
+++ Final_Project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
