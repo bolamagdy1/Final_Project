e7cbd5b8326a98243b7394369dae94656c8a4633
Abdalrhman <abdalrhmanyasser@icloud.com>
2023-07-08 16:33:21 +0300

    for zoom

--- Final_Project/Controllers/ZoomController.cs
+++ Final_Project/Controllers/ZoomController.cs
@@ -1,30 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestSharp;
 using System.Configuration;
+using static System.Net.WebRequestMethods;
 
 namespace Final_Project.Controllers
 {
+    
     public class ZoomController : Controller
     {
         //need to change path for your device
-        private readonly string TokenFilePath = "D:\\ITI\\Projects\\Grad Pro\\Final_Project\\Final_Project\\Credentials\\OauthToken.json";
+        private readonly string TokenFilePath = "D:\\Finals\\Final_Project\\Final_Project\\Credentials\\OauthToken.json";
         private string authorization_header 
         { 
             get
             {
-                var PlainTextBytes = System.Text.Encoding.UTF8.GetBytes($"{System.Configuration.ConfigurationManager.AppSettings["ClintId"]}:{System.Configuration.ConfigurationManager.AppSettings["ClintSecret"]}");
+                var PlainTextBytes = System.Text.Encoding.UTF8.GetBytes($"6UbTQISoQMq9dH9xWef8nA:AdUt4X988izvj9a8wLFOSoIVVHPRHgWM");
                 var encodedString = System.Convert.ToBase64String(PlainTextBytes);
                 return $"Basic {encodedString}";
             }
         }
         public IActionResult SignIn()
         {
-            return Redirect(string.Format("https://zoom.us/oauth/authorize?response_type=code&client_id=dwBOuoTaSt2AsjXdjCL2w&redirect_uri=http://localhost:13244/Zoom/oauthredirect", "dwBOuoTaSt2AsjXdjCL2w", "http://localhost:13244/Zoom/oauthredirect"));
+            return Redirect(string.Format("https://zoom.us/oauth/authorize?response_type=code&client_id=6UbTQISoQMq9dH9xWef8nA&redirect_uri=http://localhost:13244/Zoom/oauthredirect", "6UbTQISoQMq9dH9xWef8nA", "http://localhost:13244/Zoom/oauthredirect"));
         }
+        
         public void oAuthredirect(string code)
         {
-            //RestClient restClient = new RestClient();
-            var restClient = new RestClient("https://zoom.us/oauth/token?response_type=code&client_id=dwBOuoTaSt2AsjXdjCL2w&redirect_uri=http://localhost:13244/Zoom/oauthredirect");
+
+            var restClient = new RestClient("https://zoom.us/oauth/token");
             var request = new RestRequest();
             request.Method = Method.Post;
             request.AddHeader("Authorization", authorization_header);
@@ -36,6 +39,7 @@ namespace Final_Project.Controllers
             {
                 System.IO.File.WriteAllText(TokenFilePath, response.Content);
             }
+            //RestClient restClient = new RestClient();
             //var request = new RestRequest();
             //restClient.BaseUrl= new Uri(string.Format(System.Configuration.ConfigurationManager.AppSettings["AccessTokenUrl"],code, System.Configuration.ConfigurationManager.AppSettings["RedirectUrl"]));
             //request.AddHeader("Authorization", string.Format(authorization_header));
@@ -47,4 +51,5 @@ namespace Final_Project.Controllers
 
         }
     }
+
 }
