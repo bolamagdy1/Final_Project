68c26629d2cabea63f3d59a7add144781650ae91
Bola Magdy <bolamagdy085@gamil.com>
2023-07-08 21:16:45 +0300

    dsff

--- Final_Project/Controllers/ZoomController.cs
+++ Final_Project/Controllers/ZoomController.cs
@@ -24,43 +24,24 @@ namespace Final_Project.Controllers
         {
             return Redirect(string.Format("https://zoom.us/oauth/authorize?response_type=code&client_id=dwBOuoTaSt2AsjXdjCL2w&redirect_uri=http://localhost:13244/Zoom/oauthredirect", "dwBOuoTaSt2AsjXdjCL2w", "http://localhost:13244/Zoom/oauthredirect"));
         }
-        
+               
+            
         public void oAuthredirect(string code)
         {
-
-            var restClient = new RestClient("https://zoom.us/oauth/token");
+            
+            var restClient = new RestClient("https://zoom.us/v2/users/7dxgr5cqht@privaterelay.appleid.com/meetings");
             var request = new RestRequest();
             request.Method = Method.Post;
-
-
-            //    curl - X POST https://zoom.us/oauth/token -d 'grant_type=account_credentials'
-            //    //-d 'account_id=5yiPLwmTTpQVBnMxOlf32q' -H 'Host: zoom.us'
-            ////-H 'Authorization: Basic aGwbwxOgK6eGHEO0W1DOCv5WCODeVxoet7DFEON7bR23gP5qEW7cmeWCbCEO3ApBEWlRwCVpDWB=='
-
-            //request.AddBody();
-
-            //request.AddHeader("Host", "zoom.us");
-            //request.AddHeader("Authorization", "Basic Q2xpZW50X0lEOkNsaWVudF9TZWNyZXQ=");
-            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            //request.AddParameter("code", code);
-            //request.AddParameter("grant_type", "authorization_code");
-            //request.AddParameter("redirect_uri", "http://localhost:13244/Zoom/oauthredirect");
-
+            request.AddHeader("Authorization", authorization_header);
+            request.AddParameter("code", code);
+            request.AddParameter("redirect_uri", "http://localhost:13244/Zoom/oauthredirect");
             var response = restClient.Execute(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 System.IO.File.WriteAllText(TokenFilePath, response.Content);
             }
-            //RestClient restClient = new RestClient();
-            //var request = new RestRequest();
-            //restClient.BaseUrl= new Uri(string.Format(System.Configuration.ConfigurationManager.AppSettings["AccessTokenUrl"],code, System.Configuration.ConfigurationManager.AppSettings["RedirectUrl"]));
-            //request.AddHeader("Authorization", string.Format(authorization_header));
-            //var response = restClient.Post(request);
-            //if(response.StatusCode == System.Net.HttpStatusCode.OK)
-            //{
-            //    System.IO.File.WriteAllText(TokenFilePath,response.Content);
-            //}
+            
 
         }
     }
