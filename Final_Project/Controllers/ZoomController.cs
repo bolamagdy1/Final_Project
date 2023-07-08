09f80a044958550212b00f726b0eee7c6713bbb4
Abdalrhman <abdalrhmanyasser@icloud.com>
2023-07-08 21:12:50 +0300

    zoom changs

--- Final_Project/Controllers/ZoomController.cs
+++ Final_Project/Controllers/ZoomController.cs
@@ -23,31 +23,26 @@ namespace Final_Project.Controllers
         {
             return Redirect(string.Format("https://zoom.us/oauth/authorize?response_type=code&client_id=6UbTQISoQMq9dH9xWef8nA&redirect_uri=http://localhost:13244/Zoom/oauthredirect", "6UbTQISoQMq9dH9xWef8nA", "http://localhost:13244/Zoom/oauthredirect"));
         }
-        
+               
+            
         public void oAuthredirect(string code)
         {
-
-            var restClient = new RestClient("https://zoom.us/oauth/token");
+            
+            var restClient = new RestClient("https://zoom.us/v2/users/7dxgr5cqht@privaterelay.appleid.com/meetings");
             var request = new RestRequest();
             request.Method = Method.Post;
             request.AddHeader("Authorization", authorization_header);
             request.AddParameter("code", code);
             request.AddParameter("redirect_uri", "http://localhost:13244/Zoom/oauthredirect");
+            request.AddParameter("client_id", "6UbTQISoQMq9dH9xWef8nA");
+            request.AddParameter("client_secret", "AdUt4X988izvj9a8wLFOSoIVVHPRHgWM");
             var response = restClient.Execute(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 System.IO.File.WriteAllText(TokenFilePath, response.Content);
             }
-            //RestClient restClient = new RestClient();
-            //var request = new RestRequest();
-            //restClient.BaseUrl= new Uri(string.Format(System.Configuration.ConfigurationManager.AppSettings["AccessTokenUrl"],code, System.Configuration.ConfigurationManager.AppSettings["RedirectUrl"]));
-            //request.AddHeader("Authorization", string.Format(authorization_header));
-            //var response = restClient.Post(request);
-            //if(response.StatusCode == System.Net.HttpStatusCode.OK)
-            //{
-            //    System.IO.File.WriteAllText(TokenFilePath,response.Content);
-            //}
+            
 
         }
     }
